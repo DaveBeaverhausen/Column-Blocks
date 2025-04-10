429c8bdba776fe528b5bb10de4ae2b3b396ba74f
Josep Cortes <jcortes@rsramonsoler.com>
2025-04-10 10:02:53 +0200

    Home

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections; 
 
 public class MenuController : MonoBehaviour
 {
+    public string nextSceneName = "Home";
+    public float waitTime = 7f;
+
+    void Start()
+    {
+        StartCoroutine(ChangeSceneAfterDelay());
+    }
+
+    IEnumerator ChangeSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(waitTime);
+        GoToNextScene();
+    }
+
     public void GoToNextScene()
     {
-        SceneManager.LoadScene("Objects"); 
+        SceneManager.LoadScene(nextSceneName);
     }
 }
+
+/**
+*Usamos coroutine, que nos permite esperar el tiempo propuesto(7segundos) y cambiar de escena
+*Para usar la herramienta coroutine, usamos System.Collections
+**/
